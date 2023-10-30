8daf8d881512e8d64f5ebf004a318adaa78b9e75
dwiko100 <dwikoananda@gmail.com>
2023-10-30 23:24:12 +0700

    first commit

---
+++ dolist/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./todo-mvc-base.css";
+import "./todo-mvc-app.css";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
